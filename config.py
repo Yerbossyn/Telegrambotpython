b66455d803772701213cbb70658a84c354531a55
Yerbossyn <73870324+Yerbossyn@users.noreply.github.com>
2020-11-30 13:53:01 +0600

    Add files via upload

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 # Telegram your token
-token = ''
+token = '1433707560:AAFlfhjd7YqbDRJAHrfy38jKR59b3CFSHhk'
 
 # Privatbank API
 url = 'https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5'
